49402b7caf69ca126052b6cc0b274528b35e90a9
FershoUno <50505848+FershoUno@users.noreply.github.com>
2023-10-02 22:45:30 -0400

    Create grapejuice-installer.sh

---
+++ grapejuice-installer.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y curl
+sudo curl https://gitlab.com/brinkervii/grapejuice/-/raw/master/ci_scripts/signing_keys/public_key.gpg | sudo tee /usr/share/keyrings/grapejuice-archive-keyring.gpg > /dev/null
+sudo rm -rf /etc/apt/sources.list.d/grapejuice.list
+sudo tee /etc/apt/sources.list.d/grapejuice.list <<< 'deb [signed-by=/usr/share/keyrings/grapejuice-archive-keyring.gpg] https://brinkervii.gitlab.io/grapejuice/repositories/debian/ universal main' > /dev/null
+sudo apt update
+sudo apt install -y grapejuice
